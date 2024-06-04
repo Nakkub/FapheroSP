0047c935bb78c5838241e941924dbb59b8c86b01
Nakkub <Nebat_1@proton.me>
2024-06-04 15:28:50 -0400

    Update configwrite.py

--- configwrite.py
+++ configwrite.py
@@ -7,23 +7,44 @@ def loadconfig():
     config["General"] = {
         "Start from Last Checkpoint?": "OFF",
         "Image Breaks between Rounds?": "ON",
-        "Breaktime": 10
+        "Breaktime": 10,
 
     }
 
     config["Invasions"] = {
         "Invasion Rounds?": "ON",
+        "Randomize Invasion Chance?": "OFF",
+        "Invasion Chance Percentage": 25,
         "Invasion Rounds During Videos?": "ON",
         "Invasion Rounds During Break?": "ON",
-        "Invasion Chance Percentage": 30
+        "Multiple Invasions During Videos": "ON",
+        "Invasion Chance Increase on Checkpoint": 15
+
     }
 
     config["Modifiers"] = {
         "Modifiers?": "ON",
+        "Randomize Modifier Chance?": "OFF",
+        "Modifier Chance Percentage": 25,
         "Speed Up Modifier": "ON",
         "Squeeze Shaft Modifier": "ON",
         "Hold Breath Modifier": "ON",
-        "Modifier Chance Percentage": 10
+        "Modifier Chance Increase on Checkpoint": 10,
+    }
+
+    config["Perks"] = {
+        "Perks?": "ON",
+        "Points per Perk": 3,
+        '"Die Size Increase" Perk': "ON",
+        "Increase Die Size by?": 2,
+        '"Video Skip" Perk': "ON",
+        #'"Pause Video" Perk': "ON",
+        #"Allowed Pause Time": 10,
+        '"Invasion Decrease" Perk': "ON",
+        "Decrease Invasion Chance by what %?": 10, 
+        '"Modifier Decrease" Perk': "ON",
+        "Decrease Modifier Chance by what %?": 10
+  
     }
 
     config["Custom_File_Locations"] = {
@@ -42,7 +63,7 @@ def savedata(currentcheckpoint):
     save.optionxform = str
 
     save['Save'] = {
-        "Last Checkpoint": currentcheckpoint
+        "Last Checkpoint": currentcheckpoint,
     }
 
     with open("SaveData.txt", "w") as (my_save):
